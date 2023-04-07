02e1b02fcb4fa49b3ff286058c64538336717dbe
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-07 13:16:11 -0400

    Place all functions into `main.py` file

--- tools/main.py
+++ tools/main.py
@@ -1,3 +1,38 @@
+import pandas as pd
+
+
+def get_pandas_df(filepath: str) -> pd.DataFrame:
+    """
+    Reads a CSV file and returns a Pandas DataFrame.
+
+    Args:
+        filepath (str): The path to the CSV file.
+
+    Returns:
+        pandas.DataFrame: The DataFrame containing the CSV data.
+    """
+    df = pd.read_csv(filepath)
+    return df
+
+
+def get_df_dtypes(df: pd.DataFrame) -> dict:
+    """
+    This function takes a pandas DataFrame as input and returns a dictionary with the data types of each column.
+
+    Parameters:
+    ----------
+    df : pandas DataFrame
+        The DataFrame to analyze
+
+    Returns:
+    -------
+    dict
+        A dictionary where the keys are column names and the values are the data types of the corresponding columns.
+    """
+    dtypes = df.dtypes
+    return dict(dtypes)
+
+
 def map_pandas_to_postgres(data_types: dict) -> dict:
     """
     Maps Pandas datatype to suitable PostgreSQL datatype.
